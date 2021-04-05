373aa501bf98cec09851391504ea0827978a21ca
Josh Steward <stewardjosh10@gmail.com>
2021-04-05 11:43:40 +1000

    bug fixes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,14 +4,14 @@ const path = require("path");
 module.exports = (app) => {
     //index.html
     app.get('/', (req,res) => {
-        res.sendFile(path.join(__dirname, '..public/index.html'));
+        res.sendFile(path.join(__dirname, '../public/index.html'));
     });
     //exercise.html
     app.get('/', (req,res) => {
-        res.sendFile(path.join(__dirname, '..public/exercise.html'));
+        res.sendFile(path.join(__dirname, '../public/exercise.html'));
     });
     //stats.html
     app.get('/', (req,res) => {
-        res.sendFile(path.join(__dirname, '..public/stats.html'));
+        res.sendFile(path.join(__dirname, '../public/stats.html'));
     });
 }
