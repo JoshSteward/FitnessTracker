6892dc9972032e6f2a25bd0d3094dbe56f8293a4
Josh Steward <stewardjosh10@gmail.com>
2021-04-05 10:46:51 +1000

    Api-routes updated

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,9 +1,39 @@
+const { Workout } = require("../models");
 var db = require("../models");
 
 module.export = function(app) {
   
     //workout routes
     app.get("/api/workouts", (req,res) => {
-        
-    })
+        db.Workout.find({}, (err,workout) => {
+            if(err){
+                console.log(err);          
+            } else {
+                res.json(workout);
+            }
+        });  
+    });
+
+    //add exercise
+    app.put("/api/workouts/:id", ({body,params}, res) => {
+        Workout.findByIdAndUpdate(
+            params.id,
+            {$push: { Exercises:body }},
+            {new:true, runValidators: true}
+        )
+        .then(workout => {
+            res.json(workout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+
+    //create new workout
+    app.post("/api/workouts", (req,res) => {
+        db.Workout.create({}).then(newWorkout => {
+            res.json(newWorkout);
+        });
+    });
+
 }
